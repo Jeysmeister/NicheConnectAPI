08a680a46aeb4cc86e5d1b1cb39e92ba1b57aa53
Jayce <abenalesjc11@gmail.com>
2024-04-23 21:01:14 +0800

    users module and migrations

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { Prisma } from '@prisma/client';
 
+export const roundsOfHashing = 10;
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    if (createUserDto.password !== createUserDto.confirmPassword) {
+      throw new ConflictException('Passwords does not match');
+    }
+
+    try {
+      const hashedPassword = await bcrypt.hash(
+        createUserDto.password,
+        roundsOfHashing,
+      );
+
+      createUserDto.password = hashedPassword;
+
+      const result = await this.prisma.users.create({
+        data: {
+          username: createUserDto.username,
+          email: createUserDto.email,
+          password: createUserDto.password,
+        },
+      });
+
+      return result;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          return new ConflictException('Email already exists.');
+        } else {
+          return { error: error };
+        }
+      }
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return this.prisma.users.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    return this.prisma.users.findUnique({
+      where: { userId: id },
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    return await this.prisma.users.update({
+      where: { userId: id },
+      data: updateUserDto,
+      select: {
+        userId: true,
+        username: true,
+        email: true,
+        avatar: true,
+        bio: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    try {
+      return await this.prisma.users.delete({
+        where: { userId: id },
+      });
+    } catch (error) {
+      return { error: error };
+    }
   }
 }
